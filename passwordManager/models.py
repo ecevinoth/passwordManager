d7c3baf3de0f74ed9e3d814d5bef834a116e026a
Vinoth Chellamuthu <ecevinoth@gmail.com>
2022-10-17 10:02:26 +0800

    login completed

--- passwordManager/models.py
+++ passwordManager/models.py
@@ -1,5 +1,21 @@
 from passwordManager import db
+from passwordManager import bcrypt
 
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
+    @property
+    def password(self):
+        return self.password
+
+    @password.setter
+    def password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -7,5 +23,20 @@ class Item(db.Model):
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
     def __repr__(self):
-        return (f'Item {self.name}')
+        return f'Item {self.name}'
+
+class VM(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    hostip = db.Column(db.String(length=15), nullable=False)
+    username = db.Column(db.String(length=12), nullable=False)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+
+    @property
+    def password(self):
+        return self.password
+
+    @password.setter
+    def password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
--- passwordManager/routes.py
+++ passwordManager/routes.py
@@ -1,6 +1,8 @@
 from passwordManager import app
-from flask import render_template
-from passwordManager.models import Item
+from flask import render_template, redirect, url_for, flash
+from passwordManager.models import Item, User, VM
+from passwordManager.forms import RegisterForm, LoginForm, VMForm
+from passwordManager import db
 
 @app.route('/')
 @app.route('/home')
@@ -11,3 +13,46 @@ def home_page():
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}: #If there are not errors from the validations
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+@app.route('/vm')
+def vm_page():
+    vms = VM.query.all()
+    return render_template('vm.html', vms=vms)
+
+@app.route('/VMadd', methods=['GET', 'POST'])
+def vmadd_page():
+    form = VMForm()
+
+    if form.validate_on_submit():
+        vm_to_create = VM(hostip=form.hostip.data,
+                              username=form.username.data,
+                              password=form.password1.data)
+        db.session.add(vm_to_create)
+        db.session.commit()
+        return redirect(url_for('vm_page'))
+    if form.errors != {}: #If there are not errors from the validations
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+
+    return render_template('vmadd.html', form=form)
